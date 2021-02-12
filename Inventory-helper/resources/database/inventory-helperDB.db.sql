a50dbd4ebcd4c47f61e2b415f2eab0f3655ebb35
Yahir Eduardo Bravo <yahir.bravo@correounivalle.edu.co>
2021-02-11 23:50:36 -0500

    correction an add scheme

--- Inventory-helper/resources/database/inventory-helperDB.db.sql
+++ Inventory-helper/resources/database/inventory-helperDB.db.sql
@@ -1,63 +1,73 @@
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS `Transaction` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`product`	INTEGER,
-	`quantity`	DOUBLE,
-	`unit_price`	DOUBLE,
-	`movement`	INTEGER,
-	FOREIGN KEY(`movement`) REFERENCES `Movement`(`id`) ON DELETE SET NULL,
-	FOREIGN KEY(`product`) REFERENCES `Product`(`id`) ON DELETE SET NULL
+
+
+CREATE TABLE dealing (
+	id	INTEGER PRIMARY KEY AUTOINCREMENT,
+	product	INTEGER,
+	quantity 	DOUBLE,
+	unit_price 	DOUBLE,
+	movement 	INTEGER,
+	FOREIGN KEY( product ) REFERENCES  Product ( id ) ON DELETE SET NULL,
+	FOREIGN KEY( movement ) REFERENCES  Movement ( id ) ON DELETE SET NULL
 );
-CREATE TABLE IF NOT EXISTS `Product` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`name`	STRING ( 1 , 50 ),
-	`stock`	DOUBLE,
-	`description`	STRING ( 1 , 50 ),
-	`cpp`	DOUBLE,
-	`price`	DOUBLE,
-	`category`	INTEGER,
-	FOREIGN KEY(`category`) REFERENCES `Category`(`id`) ON DELETE SET NULL
+
+CREATE TABLE product  (
+	 id 	INTEGER PRIMARY KEY AUTOINCREMENT,
+	 name 	STRING ( 1 , 50 ),
+	 stock 	DOUBLE,
+	 description 	STRING ( 1 , 50 ),
+	 cpp 	DOUBLE,
+	 price 	DOUBLE,
+	 category 	INTEGER,
+	FOREIGN KEY( category ) REFERENCES  Category ( id ) ON DELETE SET NULL
 );
-CREATE TABLE IF NOT EXISTS `Payment` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`credit`	INTEGER,
-	`moment`	DATE,
-	`price`	DOUBLE,
-	FOREIGN KEY(`credit`) REFERENCES `Credit`(`id`)
+
+CREATE TABLE payment  (
+	 id 	INTEGER PRIMARY KEY AUTOINCREMENT,
+	 credit 	INTEGER,
+	 moment 	DATETIME,
+	 price 	DOUBLE,
+	FOREIGN KEY( credit ) REFERENCES  Credit ( id )
 );
-CREATE TABLE IF NOT EXISTS `Passwrd` (
-	`wordkwy`	STRING
+
+CREATE TABLE passwrd  (
+	 wordkwy 	STRING
 );
-CREATE TABLE IF NOT EXISTS `Movement` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`kind`	STRING,
-	`price_total`	DOUBLE,
-	`moment`	DATE
+
+
+CREATE TABLE movement  (
+	 id 	INTEGER PRIMARY KEY AUTOINCREMENT,
+	 kind 	STRING,
+	 price_total 	DOUBLE,
+	 moment 	DATETIME
 );
-CREATE TABLE IF NOT EXISTS `External_payment` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`description`	STRING,
-	`price_total`	DOUBLE,
-	`movement`	INTEGER,
-	FOREIGN KEY(`movement`) REFERENCES `Movement`(`id`) ON DELETE SET NULL
+
+CREATE TABLE external_payment  (
+	 id 	INTEGER PRIMARY KEY AUTOINCREMENT,
+	 description 	STRING,
+	 price_total 	DOUBLE,
+	 movement 	INTEGER,
+	FOREIGN KEY( movement ) REFERENCES  Movement ( id ) ON DELETE SET NULL
 );
-CREATE TABLE IF NOT EXISTS `Credit` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`client`	INTEGER,
-	`movement`	INTEGER,
-	FOREIGN KEY(`movement`) REFERENCES `Movement`(`id`) ON DELETE SET NULL,
-	FOREIGN KEY(`client`) REFERENCES `Client`(`cc`)
+
+CREATE TABLE credit  (
+	 id 	INTEGER PRIMARY KEY AUTOINCREMENT,
+	 client 	INTEGER,
+	 movement 	INTEGER,
+	FOREIGN KEY( client ) REFERENCES  Client ( cc ),
+	FOREIGN KEY( movement ) REFERENCES  Movement ( id ) ON DELETE SET NULL
 );
-CREATE TABLE IF NOT EXISTS `Client` (
-	`cc`	INTEGER,
-	`name`	STRING ( 1 , 50 ),
-	`phone`	INTEGER,
-	`address`	STRING,
-	PRIMARY KEY(`cc`)
+
+CREATE TABLE client  (
+	 cc 	INTEGER,
+	 name 	STRING ( 1 , 50 ),
+	 phone 	INTEGER,
+	 address 	STRING,
+	PRIMARY KEY( cc )
 );
-CREATE TABLE IF NOT EXISTS `Category` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`name`	STRING ( 1 , 50 ),
-	`description`	STRING ( 1 , 50 )
+
+CREATE TABLE category  (
+	 id 	INTEGER PRIMARY KEY AUTOINCREMENT,
+	 name 	STRING ( 1 , 50 ),
+	 description 	STRING ( 1 , 50 )
 );
-COMMIT;
+
